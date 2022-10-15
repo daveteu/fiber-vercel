6d5fc7eda4e9c9de08b96d7cde84172f4bb88fde
Acho Arnold <arnold@ndolestudio.com>
2022-10-15 14:53:55 +0300

    Remove logging

--- api/index.go
+++ api/index.go
@@ -1,29 +1,17 @@
 package handler
 
 import (
-	"fmt"
 	"github.com/gofiber/adaptor/v2"
 	"github.com/gofiber/fiber/v2"
-	"log"
 	"net/http"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.URL.String())
-	log.Println(r.URL.Path)
-	fmt.Println(r.URL.String())
-	fmt.Println(r.URL.Path)
 	handler().ServeHTTP(w, r)
 }
 
 func handler() http.HandlerFunc {
 	app := fiber.New()
-
-	app.Get("/", func(ctx *fiber.Ctx) error {
-		return ctx.JSON(fiber.Map{
-			"version": ctx.OriginalURL(),
-		})
-	})
 
 	app.Get("/v1", func(ctx *fiber.Ctx) error {
 		return ctx.JSON(fiber.Map{
@@ -34,6 +22,13 @@ func handler() http.HandlerFunc {
 	app.Get("/v2", func(ctx *fiber.Ctx) error {
 		return ctx.JSON(fiber.Map{
 			"version": "v2",
+		})
+	})
+
+	app.Get("/", func(ctx *fiber.Ctx) error {
+		return ctx.JSON(fiber.Map{
+			"version": ctx.Request().URI().String(),
+			"path":    ctx.Path(),
 		})
 	})
 
