20a44ac6c06edc67d657ac9b282013487555def8
Acho Arnold <arnold@ndolestudio.com>
2022-10-15 13:04:58 +0300

    Push vercel function

---
+++ api/index.go
@@ -0,0 +1,17 @@
+package handler
+
+import (
+	"github.com/gofiber/adaptor/v2"
+	"github.com/gofiber/fiber/v2"
+	"net/http"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	adaptor.FiberHandler(greet)(w, r)
+}
+
+func greet(c *fiber.Ctx) error {
+	return c.JSON(fiber.Map{
+		"success": true,
+	})
+}
